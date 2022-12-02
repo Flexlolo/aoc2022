6d8dafb86c183a22a3b3b74069e8e974471e5869
Flexlolo <Flexlololo@gmail.com>
2022-12-02 17:05:15 +0700

    day 2 afterthoughts

--- 02/02.py
+++ 02/02.py
@@ -1,60 +1,26 @@
 from lsm.aoc import get_input
 
-ROCK = 0
-PAPER = 1
-SCISSORS = 2
-
-LOSE = 0
-DRAW = 3
-WIN = 6
-
-shapes = {
-	'A': ROCK,
-	'X': ROCK,
-	'B': PAPER,
-	'Y': PAPER,
-	'C': SCISSORS,
-	'Z': SCISSORS,
-}
-
-outcomes = {
-	'X': LOSE,
-	'Y': DRAW,
-	'Z': WIN,
-}
-
 def solve(lines, part2: bool):
 	score = 0
 
 	for line in lines:
-		parts = line.split(' ')
+		elf, you = line.split(' ')
+		
+		elf = 'ABC'.index(elf)
+		result = you = 'XYZ'.index(you)
 
 		if part2:
-			elf, result = shapes[parts[0]], outcomes[parts[1]]
+			outcomes = ((elf - 1) % 3, elf, (elf + 1) % 3)
+			you = outcomes[result]
 		else:
-			elf, you = shapes[parts[0]], shapes[parts[1]]
+			outcomes = (you == (elf - 1) % 3, elf == you, you == (elf + 1) % 3)
+			result = outcomes.index(True)
 
-		if part2:
-			if result == DRAW:
-				you = elf
-			elif result == LOSE:
-				you = (elf - 1) % 3
-			else:
-				you = (elf + 1) % 3
-		else:
-			if elf == you:
-				result = DRAW
-			elif you == (elf + 1) % 3:
-				result = WIN
-			else:
-				result = LOSE
+		score += (result * 3) + you + 1
 
-		score += result + you + 1
-
-	print(f"part {'2' if part2 else '1'}:", score)
-
+	return score
 
 lines = get_input('input', day=2, year=2022).splitlines()
 
-solve(lines, False)
-solve(lines, True)
+print('PART 1:', solve(lines, False))
+print('PART 2:', solve(lines, True))
