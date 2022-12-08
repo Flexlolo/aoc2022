25c6802305c9e65cfa512590bde1aa983ebd5504
Flexlolo <Flexlololo@gmail.com>
2022-12-08 20:04:03 +0700

    final thoughts on a task

--- 08/08.py
+++ 08/08.py
@@ -1,57 +1,34 @@
 from lsm.aoc import get_input
+from lsm.aoc.space import get_directions, point_step
 from itertools import product
 
+directions = get_directions(2)
+space = {}
 
-grid = {}
+for y, line in enumerate(get_input().splitlines()):
+	for x, c in enumerate(line):
+		space[(x, y)] = int(c)
 
-for x, line in enumerate(get_input().splitlines()):
-	for y, c in enumerate(line):
-		grid[(x, y)] = int(c)
+SIZE = x + 1
 
-X = max([c[0] for c in grid]) + 1
-Y = max([c[1] for c in grid]) + 1
-assert X == Y
+def is_visible(point) -> bool:
+	def is_visible_dir(direction):
+		for p in point_step(point, direction, space):
+			if space[p] >= space[point]:
+				return False
 
-DIM = X
+		return True
 
-def is_visible(point: tuple[int, int]) -> bool:
-	x, y = point
+	return True in (is_visible_dir(direction) for direction in directions)
 
-	DIRECTIONS = {
-		(-1, 0): (range(x), None),
-		(1, 0): (range(x + 1, DIM), None),
-		(0, -1): (None, range(y)),
-		(0, 1): (None, range(y + 1, DIM)),
-	}
-
-	for rx, ry in DIRECTIONS.values():
-		rx = rx if rx else [x] * DIM
-		ry = ry if ry else [y] * DIM
-		m = max(grid[p] for p in zip(rx, ry))
-
-		if m < grid[point]:
-			return True
-
-	return False
-
-def scenic_score(point: tuple[int, int]) -> int:
-	x, y = point
-
-	DIRECTIONS = {
-		(-1, 0): (range(x-1, -1, -1), None),
-		(1, 0): (range(x + 1, DIM), None),
-		(0, -1): (None, range(y-1, -1, -1)),
-		(0, 1): (None, range(y + 1, DIM)),
-	}
-
+def scenic_score(point) -> int:
 	score = 1
 
-	for rx, ry in DIRECTIONS.values():
-		rx = rx if rx else [x] * DIM
-		ry = ry if ry else [y] * DIM
+	for direction in directions:
+		i = 0
 
-		for i, p in enumerate(zip(rx, ry)):
-			if grid[p] >= grid[point]:
+		for i, p in enumerate(point_step(point, direction, space)):
+			if space[p] >= space[point]:
 				break
 
 		score *= i + 1
@@ -61,10 +38,7 @@ def scenic_score(point: tuple[int, int]) -> int:
 invisible = 0
 best_score = 0
 
-for point in product(range(DIM), range(DIM)):
-	if True in [p == 0 or p == DIM - 1 for p in point]:
-		continue
-
+for point in product(range(SIZE), range(SIZE)):
 	if not is_visible(point):
 		invisible += 1
 
@@ -73,5 +47,5 @@ for point in product(range(DIM), range(DIM)):
 	if score > best_score:
 		best_score = score
 
-print('PART 1:', DIM ** 2 - invisible)
+print('PART 1:', SIZE * SIZE - invisible)
 print('PART 2:', best_score)
