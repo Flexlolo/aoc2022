6d9d147ae30ffcf483c584464d6a47d196f7626b
Flexlolo <Flexlololo@gmail.com>
2022-12-15 20:02:48 +0700

    really badly written task for the day, absolute dogshit

---
+++ 15/15.py
@@ -0,0 +1,75 @@
+from lsm.aoc import get_input
+from lsm.aoc.vector import Vector
+import re
+
+lines = []
+
+for line in get_input().splitlines():
+	m = re.match(r'Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)', line)
+
+	s = Vector(int(m.group(1)), int(m.group(2)))
+	b = Vector(int(m.group(3)), int(m.group(4)))
+	v = b - s
+
+	lines.append((s, b, v))
+
+# https://www.geeksforgeeks.org/merging-intervals/
+def merge_intervals(intervals) -> int:
+	intervals.sort()
+	stack = []
+	stack.append(intervals[0])
+	
+	for i in intervals[1:]:
+		if stack[-1][0] <= i[0] <= stack[-1][-1]:
+			stack[-1][-1] = max(stack[-1][-1], i[-1])
+		else:
+			stack.append(i)
+
+	return stack 
+
+def check_row(row: int, part2: bool) -> int:
+	coverage = []
+
+	if row % 10_000 == 0:
+		print('checking row', row)
+
+	for (s, b, v) in lines:
+		if (s[1] <= row and s[1] + v.mlength >= row) or (s[1] > row and s[1] - v.mlength <= row):
+			ds = v.mlength - abs(s[1] - row)
+			# print(f'{p=}, {ds=}')
+			coverage.append([s[0] - ds, s[0] + ds])
+
+	intervals = merge_intervals(coverage)
+
+	if part2:
+		if len(intervals) > 1:
+			# print(f'{row=} {intervals=}')
+
+			for i in range(len(intervals) - 1):
+				gap = intervals[i + 1][0] - intervals[i][1]
+				# print(f'{gap=}')
+
+				if gap > 1:
+					return intervals[i][1] + 1
+
+		return None
+
+	else:
+		total = 0
+
+		for r in intervals:
+			total += r[1] - r[0]
+
+		return total
+
+row_middle = 10
+row_middle = 2_000_000
+
+print('PART 1:', check_row(row_middle, False))
+
+for row in range(row_middle, row_middle * 2):
+	x = check_row(row, True)
+
+	if x is not None:
+		print('PART 2:', x * 4000000 + row)
+		break
