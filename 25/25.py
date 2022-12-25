3f96ba111722a46a1e82ab563bf8d0ed53adae98
Flexlolo <Flexlololo@gmail.com>
2022-12-25 13:04:17 +0700

    afterthoughts

--- 25/25.py
+++ 25/25.py
@@ -24,10 +24,10 @@ def find_biggest_power(value: int):
 	biggest = 0
 
 	while True:
-		biggest += 1
+		if value < 5 ** (biggest + 1):
+			return biggest
 
-		if value < 5 ** biggest:
-			return biggest - 1
+		biggest += 1
 
 def int_to_snafu(value: int) -> str:
 	magnitudes = defaultdict(int)
@@ -40,21 +40,13 @@ def int_to_snafu(value: int) -> str:
 		magnitudes[power] += whole
 		value = rem
 
-	while True:
-		done = True
-
-		for i in range(max(magnitudes.keys()) + 1):
-			if magnitudes[i] > 5:
-				magnitudes[i + 1] += magnitudes[i] // 5
-				magnitudes[i] = magnitudes[i] % 5
-				done = False
-			elif magnitudes[i] > 2:
-				magnitudes[i + 1] += 1
-				magnitudes[i] -= 5
-				done = False
-
-		if done:
-			break
+	for i in range(max(magnitudes.keys()) + 1):
+		if magnitudes[i] > 5:
+			magnitudes[i + 1] += magnitudes[i] // 5
+			magnitudes[i] = magnitudes[i] % 5
+		elif magnitudes[i] > 2:
+			magnitudes[i + 1] += 1
+			magnitudes[i] -= 5
 
 	string = ''
 
